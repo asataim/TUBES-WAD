cd07b35afc4f057f47ea53a5ac52274ef9796f3f
daniellincolnginting <148086724+daniellincolnginting@users.noreply.github.com>
2025-01-01 21:58:06 +0700

    push daniel
    fitur list account untuk megupdate user atau admin yang bisa mengakses websitenya hehe

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+<<<<<<< Updated upstream
 use App\Http\Controllers\ReportController;
+=======
+use App\Http\Controllers\AdminController;
+>>>>>>> Stashed changes
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+<<<<<<< Updated upstream
 Route::resource('profile', ProfileController::class);
 Route::get('/', [ProfileController::class, 'index']);
 Route::resource('reports', ReportController::class);
 Route::get('/', [ReportController::class, 'index']);
 
+=======
+// Route::resource('profile', ProfileController::class);
+// Route::get('/', [ProfileController::class, 'index']);
+Route::resource('admin', AdminController::class);
+Route::get('/', [AdminController::class, 'index']);
+>>>>>>> Stashed changes
